59e80779d6e15365eb47243dc19da7f0ece872f0
shishanee <mansur72095@gmail.com>
2023-07-25 11:07:58 +0300

    header is ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignIn from "./components/Sign/SignIn";
 import SignUp from "./components/Sign/SignUp";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<SignIn />} />
-      <Route path="/register" element={<SignUp />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
 
