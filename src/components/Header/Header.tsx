b4e97453198cd67b5d31a62ea4c00b73dbd529cf
shishanee <mansur72095@gmail.com>
2023-07-26 15:24:43 +0300

    CODE REVIEW

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,15 @@ import styles from "./Header.module.scss";
 import sport from "../../../public/Icon ionic-ios-fitness.png";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import ava from '../../../public/user (1).png'
-import home from '../../../public/home.png'
-import shop from '../../../public/store (1).png'
-import cart from '../../../public/shopping-cart (1).png'
-import workout from '../../../public/gym.png'
-import about from '../../../public/file.png'
+import ava from "../../../public/user (1).png";
+import home from "../../../public/home.png";
+import shop from "../../../public/store (1).png";
+import cart from "../../../public/shopping-cart (1).png";
+import workout from "../../../public/gym.png";
+import about from "../../../public/file.png";
 
 const Header = () => {
-  const token = useSelector((state) => state.application.token)
+  const token = useSelector((state) => state.application.token);
   return (
     <header>
       <div className={styles.blockDuo}>
@@ -20,29 +20,36 @@ const Header = () => {
             <h3>INTO GYM</h3>
           </div>
           <div className={styles.navbar}>
-            <div className={styles.linkNav}>
-              <img src={home} alt="" /> &nbsp;&nbsp;
-            <Link to={"/"}>HOME</Link>
-            </div>
-            <div className={styles.linkNav}>
-            <img src={shop} alt="" />&nbsp;&nbsp;
-            <Link to={"/shop"}>SHOP</Link>
-            </div>
-            <div className={styles.linkNav}>
-            <img src={cart} alt="" />&nbsp;&nbsp;
-            <Link to={"#"}>WORKOUT</Link>
-            </div>
-            <div className={styles.linkNav}>
-            <img src={workout} alt="" />&nbsp;&nbsp;
-            <Link to={"#"}>CART</Link>
-            </div>
-            <div className={styles.linkNav}>
-            <img src={about} alt="" />&nbsp;&nbsp;
-            <Link to={"/aboutUs"}>О Нас</Link>
-            </div>
+            <Link to={"/"} className={styles.linkNav}>
+              <img src={home} alt="" /> &nbsp;&nbsp; HOME
+            </Link>
+            <Link to={"/shop"} className={styles.linkNav}>
+              <img src={shop} alt="" />
+              &nbsp;&nbsp; SHOP
+            </Link>
+            <Link to={"#"} className={styles.linkNav}>
+              <img src={cart} alt="" />
+              &nbsp;&nbsp; WORKOUT
+            </Link>
+            <Link to={"#"} className={styles.linkNav}>
+              <img src={workout} alt="" />
+              &nbsp;&nbsp; CART
+            </Link>
+            <Link to={"/aboutUs"} className={styles.linkNav}>
+              <img src={about} alt="" />
+              &nbsp;&nbsp; ABOUT US
+            </Link>
           </div>
           <div className={styles.join}>
-            {!token ? <Link to={"/sign"}>GET STARTED ❯ ❯ ❯</Link>: <div className={styles.profile}> <img src={ava} alt="" /> &nbsp;&nbsp;&nbsp; <Link to={"/profile"}>Go To Profile</Link></div> }
+            {!token ? (
+              <Link to={"/sign"}>GET STARTED ❯ ❯ ❯</Link>
+            ) : (
+              <div className={styles.profile}>
+                {" "}
+                <img src={ava} alt="" /> &nbsp;&nbsp;&nbsp;{" "}
+                <Link to={"/profile"}>Go To Profile</Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
