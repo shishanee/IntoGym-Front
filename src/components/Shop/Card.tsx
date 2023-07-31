3b37a9f8553fa07200a64c277068e932fd72b037
shishanee <mansur72095@gmail.com>
2023-07-31 12:47:57 +0300

    code review

--- src/components/Shop/Card.tsx
+++ src/components/Shop/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "antd";
 import styles from "./Shop.module.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addCart } from "../../features/cartSlice";
 import { AppDispatch } from "../../app/store";
 
@@ -17,11 +17,14 @@ interface CardProps {
 
 const Cardd: React.FC<CardProps> = ({ id, image, name, price, ratingAdd }) => {
   const dispatch = useDispatch<AppDispatch>();
+  const cart = useSelector((state) => state.cart.cart);
 
   const handleClick = (id: number) => {
     ratingAdd(id);
     dispatch(addCart(id));
   };
+
+  const filtred = cart.find((item) => item._id === id);
 
   return (
     <Card
@@ -32,8 +35,8 @@ const Cardd: React.FC<CardProps> = ({ id, image, name, price, ratingAdd }) => {
     >
       <div>
         <Meta className={styles.meta} title={price} description={name} />
-        <button onClick={() => handleClick(id)} className={styles.but}>
-          В корзину
+        <button disabled={filtred} onClick={() => handleClick(id)} className={styles.but}>
+          {filtred ? "Уже в корзине" : "Добавить в корзину"}
         </button>
       </div>
     </Card>
