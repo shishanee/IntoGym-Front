558171ddb5591d2afdf688e268607b5bea37d022
Abdurrahman <timur.navrazov@mail.ru>
2023-07-31 10:56:36 +0300

    ee

--- src/components/Shop/Card.tsx
+++ src/components/Shop/Card.tsx
@@ -1,3 +1,47 @@
+// import React from 'react';
+// import { Card } from 'antd';
+// import styles from "./Shop.module.scss"
+// import { useDispatch } from 'react-redux';
+// import { addCart } from '../../features/cartSlice';
+
+// const { Meta } = Card;
+
+// interface CardProps {
+//   id: number;
+//   image: string;
+//   name: string;
+//   price: string;
+//   ratingAdd: (id: number) => void; 
+// }
+
+// const Cardd: React.FC<CardProps> = ({ id, image, name, price, ratingAdd }) => {
+//   const dispatch = useDispatch();
+  
+
+//   const handleClick = (id: number) => {
+//     ratingAdd(id);
+//     dispatch(addCart(id));
+//   };
+
+//   return (
+//     <Card
+//       className={styles.card}
+//       hoverable
+//       style={{ width: 240 }}
+//       cover={<img className={styles.photo} alt="example" src={image} />}
+//     >
+//       <div>
+//         <Meta className={styles.meta} title={price} description={name} />
+//         <button onClick={() => handleClick(id)} className={styles.but}>
+//           В корзину
+//         </button>
+//       </div>
+//     </Card>
+//   );
+// };
+
+// export default Cardd;
+
 import React from 'react';
 import { Card } from 'antd';
 import styles from "./Shop.module.scss"
@@ -6,19 +50,11 @@ import { addCart } from '../../features/cartSlice';
 
 const { Meta } = Card;
 
-interface CardProps {
-  id: number;
-  image: string;
-  name: string;
-  price: string;
-  ratingAdd: (id: number) => void; 
-}
-
-const Cardd: React.FC<CardProps> = ({ id, image, name, price, ratingAdd }) => {
+const Cardd = ({ id, image, name, price, ratingAdd }) => {
   const dispatch = useDispatch();
   
 
-  const handleClick = (id: number) => {
+  const handleClick = (id) => {
     ratingAdd(id);
     dispatch(addCart(id));
   };
